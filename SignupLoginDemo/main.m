5195d278fb80f11b7968586311c6e5e5fa9fa889
Ashish Sharma <ashishsharma@Ashishs-Mac-mini-3.local>
2014-09-04 10:43:25 +0530

    Initial Commit

---
+++ SignupLoginDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SignupLoginDemo
+//
+//  Created by Ashish Sharma on 04/09/14.
+//  Copyright (c) 2014 Ashish Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ SignupLoginDemoTests/SignupLoginDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  SignupLoginDemoTests.m
+//  SignupLoginDemoTests
+//
+//  Created by Ashish Sharma on 04/09/14.
+//  Copyright (c) 2014 Ashish Sharma. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SignupLoginDemoTests : XCTestCase
+
+@end
+
+@implementation SignupLoginDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
